0490c242902a3262e5b9530b4735f855a3018e03
ark948 <armankarimi948@gmail.com>
2024-09-06 17:33:30 +0330

    frontend: Email Verification version 02 (improved)

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -4,11 +4,12 @@ import Register from "@/components/Register.vue";
 import GetData from "@/components/GetData.vue";
 import SendData from "@/components/SendData.vue";
 import VerifyEmail from "@/components/VerifyEmail.vue";
+import VerifyEmail_v2 from "@/components/VerifyEmail_v2.vue";
 
 const routes = [
     { path: '/', component: HomeView, name: 'home' },
     { path: '/register', component: Register, name: 'register' },
-    { path: '/verify-email', component: VerifyEmail, name: 'verify_email' },
+    { path: '/verify-email', component: VerifyEmail_v2, name: 'verify_email' },
     { path: '/get-data', component: GetData, name: 'get_data' },
     { path: '/post-data', component: SendData, name: 'post_data' },
 ]
