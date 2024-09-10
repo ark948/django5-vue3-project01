a587562c2eaba6f95f93086985c816ccf4ccdc05
ark948 <armankarimi948@gmail.com>
2024-09-11 00:47:42 +0330

    Backend: Added Bookmark app to installed apps and Bookmark model + migrations

--- backend/bookmarker/models.py
+++ backend/bookmarker/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.conf import settings
 
 # Create your models here.
+
+class Bookmark(models.Model):
+    title = models.CharField("Title", max_length=32)
+    url = models.URLField("URL")
+    icon = models.ImageField("Icon")
+    owner = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='bookmarks')
+    
--- backend/server/settings.py
+++ backend/server/settings.py
@@ -51,6 +51,7 @@ INSTALLED_APPS = [
     "rest_framework_simplejwt.token_blacklist",
     "accounts.apps.AccountsConfig",
     "api.apps.ApiConfig",
+    "bookmarker.apps.BookmarkerConfig",
 ]
 
 MIDDLEWARE = [
