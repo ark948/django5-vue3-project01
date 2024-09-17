019f59e902efc646ba7c83e671ef8514e989856a
ark948 <armankarimi948@gmail.com>
2024-09-17 15:16:30 +0330

    Backend: Some slight changes in bookmarker webview

--- backend/bookmarker/webview/urls.py
+++ backend/bookmarker/webview/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 from bookmarker.webview.views import (
-    BookmarkerIndexView
+    BookmarkerIndexView,
+    BookmarksListView
 )
 
 app_name='web'
 urlpatterns = [
+    path('list', BookmarksListView.as_view(), name='list'),
     path('', BookmarkerIndexView.as_view(), name='index'),
 ]
--- backend/bookmarker/webview/views.py
+++ backend/bookmarker/webview/views.py
@@ -1,6 +1,20 @@
-from django.views.generic import TemplateView
+from typing import Any
+from django.db.models.query import QuerySet
+from django.views.generic import TemplateView, ListView
+from bookmarker.models import Bookmark
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 # views
 
 class BookmarkerIndexView(TemplateView):
     template_name = "bookmarker/index.html"
+
+class BookmarksListView(LoginRequiredMixin, ListView):
+    template_name = 'bookmarker/list_view.html'
+    model = Bookmark
+    context_object_name = 'user_bookmarks'
+
+    def get_queryset(self) -> QuerySet[Any]:
+        user = self.request.user
+        return Bookmark.objects.filter(owner=user.pk)
+
