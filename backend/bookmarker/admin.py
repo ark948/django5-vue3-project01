cc8721812a25951e0ac387bb2f5f178995692dbb
ark948 <armankarimi948@gmail.com>
2024-10-02 21:50:54 +0330

    Backend: Bookmarker app - added Category model

--- backend/bookmarker/admin.py
+++ backend/bookmarker/admin.py
@@ -1,6 +1,13 @@
 from django.contrib import admin
-from bookmarker.models import Bookmark
+from bookmarker.models import Bookmark, Category
 
 # Register your models here.
 
-admin.site.register(Bookmark)
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('title', )
+
+
+@admin.register(Bookmark)
+class BookmarkAdmin(admin.ModelAdmin):
+    list_display = ('owner', 'category')
--- backend/bookmarker/models.py
+++ backend/bookmarker/models.py
@@ -2,12 +2,24 @@ from django.db import models
 from django.conf import settings
 
 # Create your models here.
+# each bookmark can have one category
+# each category can have many bookmarks
+
+class Category(models.Model):
+    title = models.CharField("Title", max_length=60)
+    
+    class Meta:
+        verbose_name_plural = "Categories"
+
+    def __str__(self) -> str:
+        return f'Bookmarks Category: {self.title}'
 
 class Bookmark(models.Model):
     title = models.CharField("Title", max_length=120)
     url = models.URLField("URL")
     icon = models.ImageField("Icon", blank=True, null=True)
     owner = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='bookmarks')
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True, blank=True)
 
     def __str__(self):
         return f'bookmark entry for {self.owner}'
