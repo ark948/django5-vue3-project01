7fbd5a8000c82623b80db5ae34721f374e07e92c
ark948 <armankarimi948@gmail.com>
2024-10-09 16:51:06 +0330

    Backend: Removed users app

--- backend/users/utils.py
+++
@@ -1,8 +0,0 @@
-from django.utils.http import urlsafe_base64_encode
-from django.utils.encoding import force_bytes, force_str
-
-
-
-def send_email_activation_link(email, link):
-    # implement later
-    pass
