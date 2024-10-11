dab3d30316e05fbbf45d2cd644acde8c34600178
ark948 <armankarimi948@gmail.com>
2024-10-11 21:24:33 +0330

    Backend: employees view improvement

--- backend/bookmarker/api/employess_views.py
+++ backend/bookmarker/api/employess_views.py
@@ -18,7 +18,7 @@ from bookmarker.api.serializers import CategorySerializer
 from bookmarker.decorators import user_is_employee
 
 
-# no URL (only as reference)
+# not used
 @user_passes_test(user_is_employee, login_url='/bookmarker', redirect_field_name=None)
 @api_view(['GET', 'POST'])
 @permission_classes((IsAuthenticated,))
@@ -32,27 +32,28 @@ def employees_index_test_view(request) -> Response:
         data = {'categories': queryset}
         return Response(data, template_name='bookmarker/employees/index.html')
 
-    serializer = CategorySerializer(queryset, many=True)
-    data = serializer.data
-    return Response({"message": "Success"})
+    if request.accepted_renderer.format == 'json':
+        serializer = CategorySerializer(queryset, many=True)
+        data = serializer.data
+        return Response({"message": "Success"})
 
 
 
 @api_view(['GET'])
-@permission_classes([IsAuthenticated]) # works for both html and json (no need for login_requried)
+@permission_classes([IsAuthenticated])
 @renderer_classes([TemplateHTMLRenderer, JSONRenderer])
 def emp_index(request):
     """
-    index view for employees.
+    index view for employees. (HTML, JSON)
     """
     if not request.user.groups.filter(name='Employees').exists():
         return Response({'message': "You are not an employee."})
     
+    queryset = Category.objects.all()
     if request.accepted_renderer.format == 'html':
-        return Response(template_name='bookmarker/employees/index.html')
+        return Response({'categories': queryset }, template_name='bookmarker/employees/index.html')
 
-    if request.accepted_renderer.format == 'json':
-        queryset = Category.objects.all()
-        serializer = CategorySerializer(queryset, many=True)
-        data = serializer.data
-        return Response(data, status=status.HTTP_200_OK)
+    # if request.accepted_renderer.format == 'json':
+    serializer = CategorySerializer(queryset, many=True)
+    data = serializer.data
+    return Response(data, status=status.HTTP_200_OK)
